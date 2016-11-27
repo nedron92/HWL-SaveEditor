0b7e9e7df160b6bcabb151da512041b46be48561
nedron92 <becker_leinad@hotmail.com>
2016-11-27 17:55:05 +0100

    Update Core to Version 3.0.1

--- source/core/HWLSaveEditorCore.cpp
+++ source/core/HWLSaveEditorCore.cpp
@@ -47,6 +47,7 @@ vector<bool> HWLSaveEditorCore::vb_game_dlc_installed;
 
 //public members
 /* @var version  hold the current version-number as string */
+const string HWLSaveEditorCore::version = "3.0.1";
 
 
 
